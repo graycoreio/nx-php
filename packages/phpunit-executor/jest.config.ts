35d484ce8a3c7a00eb4b8bdab549ed687d290932
Damien Retzinger <damienwebdev@gmail.com>
2023-05-09 17:48:14 -0400

    test(phpunit-executor): add jest config

---
+++ packages/phpunit-executor/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'phpunit-executor',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/phpunit-executor',
+};
