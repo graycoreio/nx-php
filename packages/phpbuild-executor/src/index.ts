3fa54b37b6797493f806d3b9ec900598951cc55f
Damien Retzinger <damienwebdev@gmail.com>
2023-05-10 09:02:29 -0400

    feat(phpbuild): rework phpbuild to work without rsync

--- packages/phpbuild-executor/src/index.ts
+++ packages/phpbuild-executor/src/index.ts
@@ -4,6 +4,8 @@ import { promisify } from 'util'
 import { mkdir } from 'node:fs'
 import { join } from 'path'
 import { PhpBuildOptions } from './options'
+import { cp } from 'fs'
+import { CopyOptions } from 'fs'
 
 export default async function phpbuildExecutor(
   options: PhpBuildOptions,
@@ -16,25 +18,32 @@ export default async function phpbuildExecutor(
       throw Error('projectName must be defined.')
     }
   
+    if(context.workspace?.projects[<string>context.projectName].root === undefined) {
+      throw Error('root must be defined.')
+    }
+
     const outputPath = join(
       options.outputPath || 'vendor', 
       options.outputName || context.projectName.replace('@',''),
-    );
-    
-    const { stderr, stdout } = await promisify(exec)(
-      ` mkdir -p ${outputPath} && \
-        rsync -r \
-        ${context.workspace?.projects[<string>context.projectName].root}/** \
-        ${outputPath} \
-        --exclude Test && composer dump-autoload
-      `
     )
-     
+
+    const cpPromise = <(src: string, dest: string, opt: CopyOptions) => Promise<void>><unknown>promisify(cp); 
+    
+    const { stderr, stdout } = await 
+      promisify(mkdir)(outputPath, { recursive: true })
+      .then(() => cpPromise(
+        context.workspace?.projects[<string>context.projectName].root || '',
+        outputPath,
+        { recursive: true },
+      )
+      .then(() => promisify(exec)('composer dump-autoload'))
+    )
 
     console.log(stdout)
     console.error(stderr)
     return { success: !stderr }
   } catch (e: any) {
+    console.log(e)
     console.log(e.stdout)
     console.error(e.stderr)
     return { success: false }
