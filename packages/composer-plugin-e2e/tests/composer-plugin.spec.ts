b5d05d627b4beab87886eae8b8fce632fbfbcabf
Damien Retzinger <damienwebdev@gmail.com>
2023-05-14 16:20:36 -0400

    feat(composer-plugin): only check project folders
    Previously, we checked anything in `packages`.
    We now check all project folders for a composer.json.

--- packages/composer-plugin-e2e/tests/composer-plugin.spec.ts
+++ packages/composer-plugin-e2e/tests/composer-plugin.spec.ts
@@ -1,8 +1,12 @@
 import {
   ensureNxProject,
+  readFile,
+  readJson,
   runNxCommandAsync,
-  uniq,
+  tmpProjPath,
+  updateFile,
 } from '@nrwl/nx-plugin/testing';
+import * as fs from 'fs';
 
 describe('composer-plugin e2e', () => {
   // Setting up individual workspaces per
@@ -11,8 +15,15 @@ describe('composer-plugin e2e', () => {
   // consumes 1 workspace. The tests should each operate
   // on a unique project in the workspace, such that they
   // are not dependant on one another.
-  beforeAll(() => {
+  beforeAll(async () => {
     ensureNxProject('composer-plugin', 'dist/composer-plugin');
+
+    await fs.cp('testworkspace/packages', tmpProjPath('libs'), { recursive: true }, () => { });
+    updateFile('nx.json', (originalContent: string) => {
+      const nx = JSON.parse(originalContent);
+      nx.plugins = ['composer-plugin'];
+      return JSON.stringify(nx);
+    });
   });
 
   afterAll(() => {
@@ -21,10 +32,14 @@ describe('composer-plugin e2e', () => {
     runNxCommandAsync('reset');
   });
 
-  it('should create composer-plugin', async () => {
-    const project = uniq('composer-plugin');
-    await runNxCommandAsync(`generate composer-plugin:composer-plugin ${project}`);
-    const result = await runNxCommandAsync(`build ${project}`);
-    expect(result.stdout).toContain('Executor ran');
-  }, 120000);
+  it('should build php packages in the correct order', async () => {
+    await runNxCommandAsync('reset');
+    const result = await runNxCommandAsync('graph --file=graph.json');
+    const graph: any = readJson('graph.json');
+
+    expect(graph.graph.dependencies['nx-php/non-php-package']).toEqual([]);
+    expect(graph.graph.dependencies['nx-php/package-a']).toEqual([]);
+    expect(graph.graph.dependencies['nx-php/package-b']).not.toEqual([]);
+    expect(graph.graph.dependencies['nx-php/package-c']).not.toEqual([]);
+  });
 });
--- packages/composer-plugin/src/index.ts
+++ packages/composer-plugin/src/index.ts
@@ -4,7 +4,6 @@ import {
     ProjectGraphProcessorContext
 } from '@nrwl/devkit'
 
-import * as glob from 'glob'
 import * as fs from 'fs'
 
 export function processProjectGraph(
@@ -12,25 +11,36 @@ export function processProjectGraph(
     context: ProjectGraphProcessorContext
 ): ProjectGraph {
     const builder = new ProjectGraphBuilder(graph)
-    
-    addComposerPackagesToGraphBuilderFromPath(builder, 'packages');
+
+    Object.keys(context.projectsConfigurations.projects).map((key) => {
+        if (context.projectsConfigurations.projects[key].root) {
+            addComposerPackagesToGraphBuilderFromPath(
+                builder, context.projectsConfigurations.projects[key].root
+            );
+        }
+    });
 
     return builder.getUpdatedProjectGraph()
 }
 
-export const addComposerPackagesToGraphBuilderFromPath = (builder: any, path: string) => {
-    const composerPackages = glob.sync(path + '/*/composer.json').map((file) => JSON.parse(fs.readFileSync(file).toString()));
-    console.log(composerPackages);
-
-    for (const composerPackage of composerPackages) {
-        if(!composerPackage.require){
-            //skip if we have no dependencies.
-            continue
-        }
-
-        for (const dep of Object.keys(composerPackage.require)) {         
-            console.log(dep);   
-            builder.addImplicitDependency(composerPackage.name, dep)
-        }
+export const addComposerPackagesToGraphBuilderFromPath = (builder: any, path: string): void => {
+    try {
+        fs.statSync(path + '/composer.json', { throwIfNoEntry: true })
     }
+    catch(e) {
+        return
+    }
+    
+    const library = JSON.parse(fs.readFileSync(path + '/composer.json').toString());
+
+    if (!library.require) {
+        //skip if we have no dependencies.
+        return;
+    }
+
+    for (const dep of Object.keys(library.require)) {
+        builder.addImplicitDependency(library.name, dep)
+    }
+
+    return;
 }
